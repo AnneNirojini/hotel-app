05176cced12a63be720477e2ca02862e16d9358e
unknown <nirojiniashokkumar@gmail.com>
2024-12-17 17:11:42 +0530

    project

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homescreen from './screens/Homescreen';
+import Bookingscreen from './screens/Bookingscreen';
+import Registerscreen from './screens/Registerscreen';
+import Loginscreen from './screens/Loginscreen';
+import SuccessScreen from './screens/SuccessScreen';
+import Profilescreen from './screens/Profilescreen';
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/home" element={<Homescreen />} />
+                    {/* Pass hallid as a route parameter */}
+                    <Route path="/booking/:hallid/:fromdate/:todate" element={<Bookingscreen />} />
+                    <Route path='/register' element={< Registerscreen/>}/>
+                    <Route path='/login' element={< Loginscreen/>}/>
+                    <Route path="/success" element={< SuccessScreen/>} /> 
+                    <Route path='/profile' exact Component={Profilescreen} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
